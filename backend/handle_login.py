9ca25d99b850a48e3c760508a181eb1a7601c13b
hiperbolt <tomasimoes03@gmail.com>
2022-04-30 16:58:01 +0100

    More routes

---
+++ backend/handle_login.py
@@ -0,0 +1,45 @@
+from passlib.hash import pbkdf2_sha256
+import bcrypt
+import jwt
+from datetime import datetime, timedelta
+import config
+from flask import request
+
+iterations = 6
+
+
+
+def loginHandler(client, username, password):
+    response = {
+        "success" : False,
+        "message" : "Invalid parameters",
+        "token" : ""
+    }
+
+    if not username or not password:
+        return response
+
+    query = ('SELECT hash_, salt FROM `eth-348810.test_dataset.credentials` WHERE username = \'' + username + '\'')
+    query_job = client.query(query)
+    rows = query_job.result()
+    for row in rows: # There should always be only one row, since usernames are unique
+        for (hash, salt) in row:
+            if hash == pbkdf2_sha256.hash(password, salt):
+                response["success"] = True
+    
+    if not response["success"]:
+        response["message"] = "Unauthorized"
+        return response
+
+    #If the login was sucessful, we generate a token.
+
+    response["token"] = jwt.encode({
+                'username': username,
+                'exp': datetime.utcnow() + timedelta(minutes=30)
+            }, config.SECRET_KEY)
+
+    response["message"] = "OK"
+    response["success"] = True
+
+    return response
+
--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-from flask_bcrypt import Bcrypt
-from flask_login import (
-    UserMixin,
-    login_user,
-    LoginManager,
-    current_user,
-    logout_user,
-    login_required,
-)
-
-import grabByID, returnPage, menuEntries, searchByTerm
+import grabByID, returnPage, menuEntries, searchByTerm, handle_login, validateToken, appTitle
 from google.cloud import bigquery
 
 app = Flask(__name__)
@@ -20,7 +10,7 @@ client = bigquery.Client()
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return "Welcome to 3eth's backend"
 
 @app.route('/api/v1/publicQuery/<id>', methods=['GET'])
 def grabID(id):
@@ -30,11 +20,26 @@ def grabID(id):
 def returnPageByIndex(index, numElements):
     return returnPage.grab(index, numElements, client)
 
-@app.route('/api/v1/publicQuery/searchTerm', methods=['GET'])
+@app.route('/api/v1/publicQuery/<searchTerm>', methods=['GET'])
+def searchByTerm(searchTerm):
+    return searchByTerm.returnSearchByTerm(client)
 
 @app.route('/api/v1/ui/menuEntries', methods=['GET'])
 def returnMenuEntries():
     return menuEntries.returnEntries(client)
+
+@app.route('/api/v1/ui/appTitle', methods=['GET'])
+def getAppTitle():
+    return appTitle.grab(client)
+
+@app.route('/api/v1/private/getToken/<username>/<password>', methods=['GET'])
+def returnAuthToken(username, password):
+    return handle_login.loginHandler(client, username, password)
+
+@app.route('/api/v1/private/validateToken', methods=['GET'])
+def returnTokenValidation():
+    return validateToken.grab(client)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
