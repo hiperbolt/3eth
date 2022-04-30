36a3fa18ed3d72b1b892c0628bfe0d82d93f6aab
hiperbolt <tomasimoes03@gmail.com>
2022-04-30 22:50:31 +0100

    Presentation and important smiley added

--- backend/routes/grabByID.py
+++
@@ -1,4 +0,0 @@
-def grab(id, client):
-    query = "SELECT * FROM 'bigquery-public-data.hacker_news.full' WHERE id = {}".format(id)
-    query_job = client.query(query)
-    return query_job.result()
